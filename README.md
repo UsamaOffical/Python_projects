8e40b393917148ce5904098b1928ceab2e8f6b07
Usama Jameel <160001523+UsamaOffical@users.noreply.github.com>
2025-03-06 22:32:46 +0000

    todo list not complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Python_projects
+
+## This command active virtual enviroment in Git bash 
+
+'''source .venv/bin/activate'''
+
+
+
--- Todo-list/todo-cli/todo.py
+++ Todo-list/todo-cli/todo.py
@@ -29,7 +29,46 @@ def add(task):
     save_tasks(tasks)
     click.echo(f'Task added successfully: {task}!')
 
+
+@click.command()
+def list():
+    '''List the all tasks'''
+    tasks = load_tasks()
+    if not tasks:
+        click.echo('No task Found')
+    for index, task in enumerate(task,1):
+        status ='✅' if task['done'] else '❌'
+        click.echo(f'{index} {task['task'] [{status}]}')
+
+@click.command()
+@click.argument('task_number' , type=int)
+def complete(task_number):
+    '''Marks a tasks completed'''
+    tasks = load_tasks()
+    if 0 < task_number <= len(tasks):
+        tasks[tasks - 1]['done'] = True
+        save_tasks(task_number)
+        click.echo(f'Tasks {task_number} marked as completed!')
+    else:
+        click.echo(f'invalid task number {task_number}')    
+
+@click.command()
+@click.argument('task_number', type=int )
+def remove(task_number):
+    '''Remove the task from the list'''
+    tasks = load_tasks()
+    if 0 < task_number <= len(tasks):
+        remove_task = tasks.pop( task_number - 1 )
+        save_tasks(tasks)
+        click.echo(f'Removed task :{remove_task['task']}')
+    else:
+        click.echo('Invalid task number')
+
 cli.add_command(add)
+cli.add_command(list)
+cli.add_command(complete)
+cli.add_command(remove)
+
 
 if __name__ == '__main__':
     cli()

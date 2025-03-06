083ec8eb2b6cd08bbf24c50326a9a9e850015915
Usama Jameel <160001523+UsamaOffical@users.noreply.github.com>
2025-03-06 21:29:14 +0000

    psuh

---
+++ Todo-list/todo-cli/todo.py
@@ -0,0 +1,36 @@
+import click
+import os
+import json
+
+TODO_FILE = 'todo.json'
+
+def load_tasks():
+    if os.path.exists(TODO_FILE):
+        return []
+    with open(TODO_FILE,'r') as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(TODO_FILE,'w') as file:
+        json.dump( tasks, file, indent=4 ) 
+
+
+@click.group()
+def cli():
+    '''Simple Todo List Manager'''
+    pass
+
+@click.command()
+@click.argument('task')
+def add(task):
+    '''Add a new task ro the list'''
+    tasks = load_tasks()
+    tasks.append({'task':task, 'done':False})
+    save_tasks(tasks)
+    click.echo(f'Task added successfully: {task}!')
+
+cli.add_command(add)
+
+if __name__ == '__main__':
+    cli()
+    
